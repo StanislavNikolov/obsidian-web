25106935f1f3cc5b93e87125674e4a0b085d2121
mt01212 <mt01212@surrey.ac.uk>
2021-03-17 15:26:53 +0200

    add dotenv;

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const env = require('dotenv');
 const app = express();
 
-const port = 3000 || env.PORT;
+const port = env.PORT || 3000;
 
 app.use(express.static('static'));
-
 app.use(express.static('node_modules/matter-js/build'));
 
 
